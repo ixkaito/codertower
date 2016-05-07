7021ee77b6608c4c54c4ca5ae25b53929df8c79d
Kite <ixkaito@gmail.com>
2016-05-08 06:49:07 +0900

    Display the player

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,9 +2,21 @@ enchant();
 
 window.onload = function() {
 
-  var game = new Core(320, 320);
+  var game = new Core(window.innerWidth, window.innerHeight);
   game.fps = 15;
+  game.preload('./assets/images/knight.png', './assets/images/green-slime.png');
   game.onload = function() {
+
+    var player = new Sprite(32, 32);
+    player.x = 0;
+    player.y = 0;
+    var playerImage = new Surface(288, 128);
+    playerImage.draw(game.assets['./assets/images/knight.png'], 0, 0, 288, 128, 0, 0, 288, 128);
+    player.image = playerImage;
+
+    var stage1 = new Group();
+    stage1.addChild(player);
+    game.rootScene.addChild(stage1);
 
   };
 
